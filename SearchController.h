2edb9816b00974b0322efe0b50617eb606bc7031
Stefano Pigozzi <slicer1337@gmail.com>
2009-08-02 16:40:32 +0200

    Added SearchController.

---
+++ SearchController.h
@@ -0,0 +1,16 @@
+//
+//  SearchController.h
+//  iMAL
+//
+//  Created by Stefano Pigozzi on 8/2/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SearchController : NSObject {
+
+}
+
+@end
---
+++ SearchController.m
@@ -0,0 +1,14 @@
+//
+//  SearchController.m
+//  iMAL
+//
+//  Created by Stefano Pigozzi on 8/2/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "SearchController.h"
+
+
+@implementation SearchController
+
+@end
